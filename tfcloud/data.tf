3d5879b7492a3fa3ed61196185ccb173205ef539
Julián Camilo Cuevas A <akath19@gmail.com>
2025-07-12 15:14:08 -0500

    Added cloudflare-dns workspace

---
+++ tfcloud/data.tf
@@ -0,0 +1,3 @@
+data "tfe_github_app_installation" "github-app" {
+  installation_id = var.github-app-id
+}
--- tfcloud/workspaces.tf
+++ tfcloud/workspaces.tf
@@ -1,11 +1,22 @@
-data "tfe_github_app_installation" "github-app" {
-  installation_id = var.github-app-id
-}
-
 resource "tfe_workspace" "tfcloud-workspaces" {
   name                  = "tfcloud-workspaces"
   organization          = var.organization
   working_directory     = "tfcloud"
+  auto_apply            = true
+  file_triggers_enabled = true
+  queue_all_runs        = true
+
+  vcs_repo {
+    branch                     = "main"
+    identifier                 = var.github-repo
+    github_app_installation_id = data.tfe_github_app_installation.github-app.id
+  }
+}
+
+resource "tfe_workspace" "cloudflare-dns" {
+  name                  = "cloudflare-dns"
+  organization          = var.organization
+  working_directory     = "cf-dns"
   auto_apply            = true
   file_triggers_enabled = true
   queue_all_runs        = true
