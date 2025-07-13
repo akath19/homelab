882421ef688ef890161384ff13edf160fe2facf0
Julian Cuevas <akath19@gmail.com>
2025-07-12 19:05:42 -0500

    Added custom VPC & subnet in main region (#14)

--- gcp/variables.tf
+++ gcp/variables.tf
@@ -1,3 +1,8 @@
+variable "project" {
+  type        = string
+  description = "Project ID to create resources in"
+}
+
 variable "region" {
   type        = string
   description = "Region to create resources in"
